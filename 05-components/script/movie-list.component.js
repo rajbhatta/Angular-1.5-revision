d41ced94e400a753b8a903d20172b745cb7e7878
Raj Bhatta <bhattaraj21@gmail.com>
2020-11-22 15:16:42 -0800

    Angular js component

---
+++ 05-components/script/movie-list.component.js
@@ -0,0 +1,9 @@
+(function(){
+        "use strict"
+
+        var module=angular.module("myModule",[]);
+        
+        module.component("movieList", {
+            template: "Hello from a component"
+        });
+});

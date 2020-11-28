fc63a11157afb0014cc4d840f15dad551f01a0ba
Raj Bhatta <bhattaraj21@gmail.com>
2020-11-28 13:47:53 -0800

    How scope and controller work

--- 08-scope-and-controller/script/script.js
+++ 08-scope-and-controller/script/script.js
@@ -1,7 +1,16 @@
 //Step 1: Create a module
 var myApp=angular.module("myModule",[]);
 
-myApp.controller("emp", function($scope){
-    $scope.message="Hello Raj";
-});
+myApp.controller("emp", function ($scope,$log){
 
+    //1st function
+    $scope.search = function () {
+        $log.log('Hello search is clicked')
+    };
+
+    //2nd function
+    $scope.click = function () {
+        $log.log('Hello table is clicked')
+    };
+
+})

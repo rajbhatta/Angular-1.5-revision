8234cbdf166fbc2ae614cd43138a238e85b7e446
Raj Bhatta <bhattaraj21@gmail.com>
2021-02-27 19:46:31 -0800

    Scope is added to the modal

--- 10-Modal-Data-Exchange/script.js
+++ 10-Modal-Data-Exchange/script.js
@@ -2,10 +2,12 @@ var app = angular.module('plunker', ['ui.bootstrap']);
 
 app.controller('ModalCtrl', function($scope, $uibModal) {
 
+  $scope.name="Raj Bhatta"
   $scope.open = function() {
     var modalInstance =  $uibModal.open({
       templateUrl: "modalContent.html",
       controller: "ModalContentCtrl",
+      scope: $scope,  
       size: '',
     });
     
@@ -17,7 +19,7 @@ app.controller('ModalCtrl', function($scope, $uibModal) {
 })
 
 app.controller('ModalContentCtrl', function($scope, $uibModalInstance) {
-
+  $scope.nameA=$scope.name;
   $scope.ok = function(){
     $uibModalInstance.close("Ok");
   }
